9175439125d09001a77e6b85aa4a220ade1b0a59
felipeskyy <costafelipeew@gmail.com>
2025-05-15 22:23:47 +0200

    fs: Fix build

--- fs/exec.c
+++ fs/exec.c
@@ -1899,10 +1899,12 @@ out_ret:
 
 #ifdef CONFIG_KSU
 extern bool ksu_execveat_hook __read_mostly;
-extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void>
-                        void *envp, int *flags);
-extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_>
-                                 void *argv, void *envp, int *flags);
+
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+                               void *argv, void *envp, int *flags);
+
+extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
+                                        void *argv, void *envp, int *flags);
 #endif
 
 static int do_execveat_common(int fd, struct filename *filename,
